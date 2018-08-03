8521d04510fb44aa92a8e466ba85aa3c4c33091e
meystr <meystr@gmail.com>
2018-08-03 21:57:26 +0200

    w

---
+++ src/square.java
@@ -0,0 +1,13 @@
+public class square {
+    int i;
+
+    int kwadrat(int i){
+        return i * i;
+    }
+
+public static void main(String args[]){
+    square mybox = new square();
+    //mybox.kwadrat(5);
+    System.out.print(mybox.kwadrat(5));
+}
+}
